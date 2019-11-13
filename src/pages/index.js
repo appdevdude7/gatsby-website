f10c00c14920cd5b2520377f34d49b866aa102c8
Akshay R <46285388+appdevdude7@users.noreply.github.com>
2019-11-13 07:20:40 -0600

    added content

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,21 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>This is a Gatsby Project</h1>
+    <h1>Lets discuss about Baseball</h1>
     <p>Let's play with it</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       {/* <Image /> */}
       <img src={baseball} alt="Logo" />
+      <p>
+        Baseball is a medium to express yourself as a sportsman. It is one of
+        the most followed games in the United States and also has a huge
+        popularity worldwide.
+        <a href="https://baseballd.com">Baseball bats</a> most definitely has a
+        variety of roles. Every one's dream is to become one of the finest
+        hitters and have a homerun. Be it fastpitch, slowpitch or big barrel
+        game, baseball bats always make a huge difference in the way you strike
+        the ball. Not to undermine the technique of the player and the timings.
+      </p>
     </div>
     <Link to="/page-2/">Go to page 2 </Link>
     <Link to="/about/"> Go to About</Link>
