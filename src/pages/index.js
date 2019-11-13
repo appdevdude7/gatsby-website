8180dac09f427eb1cc083febb3672aef43411b0b
Akshay R <46285388+appdevdude7@users.noreply.github.com>
2019-11-13 06:50:09 -0600

    Style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
+import baseball from "../images/baseball-bags.jpg" // Tell Webpack this JS file uses this image
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+/* import Image from "../components/image" */
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,7 +12,8 @@ const IndexPage = () => (
     <h1>This is a Gatsby Project</h1>
     <p>Let's play with it</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
+      <img src={baseball} alt="Logo" />
     </div>
     <Link to="/page-2/">Go to page 2 </Link>
     <Link to="/about/"> Go to About</Link>
