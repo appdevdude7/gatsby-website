8466e3584761e01a56faca4d177890baa32ab359
Akshay R <46285388+appdevdude7@users.noreply.github.com>
2019-11-13 07:08:36 -0600

    added form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,55 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Contact = () => (
+export default class Contact extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Contact Page" />
+        <h1>Hi from the Contact page</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            First name
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Last name
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
+/* const Contact = () => (
   <Layout>
     <SEO title="Contact Page" />
     <h1>Hi from the Contact page</h1>
@@ -13,3 +61,4 @@ const Contact = () => (
 )
 
 export default Contact
+ */
